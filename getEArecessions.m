1f217ca18d24d3d742bed35e0565592e0fa9eece
andreadepolis <andrea.depolis.17@mail.wbs.ac.uk>
2022-08-13 18:39:16 +0200

    Efficiency improved

--- getEArecessions.m
+++ getEArecessions.m
@@ -1,4 +1,3 @@
-
 function Recessions = getEArecessions()
 % The function retrieves the most recent EABCN recession announcement and
 % computes the relavitve datenum values. Requires a network connection.
@@ -25,33 +24,24 @@ subtree1 =  findElement(readHTML, "A") ;
 subtree2 = getAttribute(subtree1, "href") ;
 
 getlink  = subtree2(contains(subtree2, 'cepr_recession_indicator')) ;
+recdates = webread(getlink) ;
 
-ceprdates = webread(getlink) ;
+dates = recdates.Dates ;
+recid = logical(recdates.PeakExcluded) ;
 
-dates = ceprdates.Dates ;
-recid = logical(ceprdates.PeakExcluded) ;
+q2m = @(x) 3*(x == 0) + 6*(x == .25) + 9*(x == .5) + 12*(x == .75) ;
 
-q2m = @(x) 1*(x == 0) + 4*(x == .25) + 7*(x == .5) + 10*(x == .75) ;
+yr = fix(dates) ;
+mt = q2m(mod(dates, 1)) ;
+t  = datetime(yr, mt, 1) ;
 
-y1 = fix(dates(1)) ;
-m1 = q2m(mod(dates(1), 1)) ;
+numrec(recid) = datenum(t(recid)) ;
 
-yT = fix(dates(end)) ;
-mT = q2m(mod(dates(end), 1)) ;
-
-t = (datetime(y1, m1, 1) : calquarters(1) : datetime(yT, mT, 1))' ;
-numrec(recid, 1) = datenum(t(recid)) ;
-
-p = find(numrec ~= 0) ;
+p =   find(numrec ~= 0) ;
 D = numrec(numrec ~= 0) ;
 
 p2 = find(diff(p) ~= 1) ;
 
-P1 = D([1 p2' + 1]) ;
-P2 = D([p2' end]) ;
+Recessions(:, 1) = D([1 p2+1]) ;
+Recessions(:, 2) = D([p2 end]) ;
 
-C = zeros(size(P1, 1), 2) ;
-C(:, 1) = P1 ;
-C(:, 2) = P2 ;
-
-Recessions = C ;
